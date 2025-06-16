2eaad5f5d659bd7da4b0797e78b6fcd9ae28b02d
Abhishek-Gadhave <84014345+Abhishek-Gadhave@users.noreply.github.com>
2025-06-16 09:49:32 +0000

    data types and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1349544;
+let accountEmail = "abhishekg@incredmoney.com";
+let accountPassword =  "12345";
+acccountCity = "Mumbai";
+let accountState;
+
+accountEmail = "gg@incredmoney.com"
+accountPassword = "324322"
+acccountCity = "pune"
+//accountId = 212333;
+console.log(accountId);
+console.table ([accountId,accountEmail,accountPassword,acccountCity,accountState])
+
+/* prefer not to use var 
+because issue in block scope and functional scope*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"  // it will treat all code as a newer version
+
+//alert( 3+3) //we are using node.js not browser
+
+console.log(3
+    +3); //code readability should be high
+    
+console.log(4+4)
+
+let name = "Abhishek";
+let age = 27;
+let isLoggedIn = "true"
+
+//primitive data type
+//number  => 2 to the power 53
+//bigint
+//string => ""
+//bolean => true/false;
+//null => it is a standalone value
+// undefined => 
+//symbol => to find uniqueness we use symbol
+
+
+//Non-primitive data types
+//Object
+console.log(typeof "");
+console.log(typeof age);
+console.log(typeof null);//output => object // type of null is object
+console.log(typeof undefined)
