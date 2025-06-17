c58898a2d4d3253269bb08329e955e8c84bbc985
Abhishek-Gadhave <84014345+Abhishek-Gadhave@users.noreply.github.com>
2025-06-17 06:40:18 +0000

    Type conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+let score = "hitesh";
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//"true" => 1; false => 0;
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+let UserLoggedIn = true;
+const isUserLoggedIn = Number(UserLoggedIn);
+//console.log(isUserLoggedIn)
+
+
+let islogOut = 0;
+let logOut = Boolean(islogOut);
+console.log(typeof (logOut))
+
+//0=>false 1=>true
+//""=>false "abhishek" => true
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
